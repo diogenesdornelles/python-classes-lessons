dc90cfb21efd5b6e24e114d7f0158ad008340883
Diógenes Dornelles Costa <101067755+diogenesdornelles@users.noreply.github.com>
2023-07-27 20:18:22 -0300

    Update main.py

--- Bilateral Association/main.py
+++ Bilateral Association/main.py
@@ -1,6 +1,18 @@
 from modules import Person
 from modules import House
 
+# In object-oriented programming, the term "bilateral association" typically refers to an association between two classes where both classes have a relationship with each other. 
+# This means that both classes are aware of each other and can interact directly.
+# There are two common types of bilateral associations:
+# One-to-One Bilateral Association: In this type of association, each instance of one class is associated with exactly one instance of another class, and vice versa. 
+# For example, consider two classes: Person and Passport. Each person can have only one passport, and each passport belongs to only one person. So, there's a one-to-one bilateral association between Person and Passport.
+# One-to-Many Bilateral Association: In this type of association, each instance of one class is associated with multiple instances of another class, and vice versa. 
+# For example, consider two classes: Department and Employee. A department can have many employees working for it, and each employee works in a single department. 
+# So, there's a one-to-many bilateral association between Department and Employee.
+# Bilateral associations can be established using instance variables, where one class holds a reference to the other class, or through method parameters and return values that allow communication between the classes.
+# It's important to design and manage bilateral associations carefully, as they can introduce tighter coupling between classes, and changes in one class may impact the other class. 
+# In some cases, it might be more appropriate to use unidirectional associations or event-based communication to reduce coupling and improve code maintainability.
+
 # https://www.youtube.com/watch?v=OXu-Q2NoS-Y&list=PLAgbpJQADBGLo24x_xBwGtTDO-bjwrFb_&index=12
 # Orientação a Objetos em Python — Associação Bilateral
 # Instantiate objects.
