6ef10c1b39094ab7fb2e0a8c3b5062efbbe47ff4
Diógenes Dornelles Costa <101067755+diogenesdornelles@users.noreply.github.com>
2023-07-27 21:14:36 -0300

    Update main.py

--- Classes Composition/main.py
+++ Classes Composition/main.py
@@ -2,6 +2,14 @@ from dataclasses import dataclass
 from inputs import choose_one_operation, inform_id
 from repository import Repository
 
+
+# Composition is an association relationship between classes in object-oriented programming, where one class (the container or composite) contains or is composed of other classes (the components or parts). 
+# The composition relationship implies a strong "whole-part" relationship, where the lifecycle of the component objects is tightly tied to the lifecycle of the container. In other words, if the container is destroyed, all of its component objects are also destroyed.
+# Composition is a more specific and stronger form of association compared to aggregation. In composition, the component objects are considered an integral part of the container, and they cannot exist independently outside of it. This means that the container class is responsible for creating, managing, and destroying its component objects.
+# In UML (Unified Modeling Language), composition is typically represented by a diamond-shaped arrowhead on the side of the container class pointing towards the component class, similar to aggregation.
+# The difference lies in the strength of the relationship and the lifecycle management of the component objects.
+
+
 # https://www.youtube.com/watch?v=px7UphXv4B8&list=PLAgbpJQADBGLo24x_xBwGtTDO-bjwrFb_&index=22
 # Orientação a Objetos em Python — Composição de Classes
 
